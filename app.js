00ea06defc45565fe7f1fb19b219f116e963f1bd
Brian Spencer <brsj13@gmail.com>
2020-08-28 23:36:41 -0700

    refactor through chained route handlers

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res) {
+app.route("/articles")
+
+.get(function(req, res) {
     Article.find(function(err, foundArticles) {
         if (!err){
             res.send(foundArticles);
@@ -28,9 +30,9 @@ app.get("/articles", function(req, res) {
             res.send(err);
         }
     });
-});
+})
 
-app.post("/articles", function(req, res){
+.post(function(req, res){
 
     const newArticle = new Article({
         title: req.body.title,
@@ -45,9 +47,9 @@ app.post("/articles", function(req, res){
             res.send(err);
         }
     });
-});
+})
 
-app.delete("/articles", function(req, res){
+.delete(function(req, res){
     Article.deleteMany(function(err){
         if (!err){
             res.send("Successfully deleted all articles.")
